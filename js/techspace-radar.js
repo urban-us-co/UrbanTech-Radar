2f32fa9e7b04af9f71921e49c86881ef254cdc85
Stonly Baptiste <stonlyb@gmail.com>
2015-12-30 10:33:40 -0800

    update to company popup

--- js/techspace-radar.js
+++ js/techspace-radar.js
@@ -33,10 +33,12 @@ function loadData() {
         problemareas = _.map(problemareas, function(e) {
             return e.data.sort(function(e, t) {
                 if (e["Business Model"].length > 1) {
-                    if(e["Business Model"].indexOf("B2C") > 0 ){
-                        var ind = e["Business Model"].indexOf("B2C");
+                    if(e["Business Model"].indexOf("B2C") > -1 ){
+                        e["Business Model"] = ["B2C"];
+                        return 0;
+                        /*var ind = e["Business Model"].indexOf("B2C");
                         e["Business Model"].splice(ind, 1);
-                        e["Business Model"].unshift("B2C");
+                        e["Business Model"].unshift("B2C");*/
                     }
                     if (e["Business Model"][0] === "B2C" && e["Business Model"][1] === "B2B") return -1;
                     if (e["Business Model"][0] === "B2C" && e["Business Model"][1] === "B2G") return 1;
@@ -289,6 +291,14 @@ function showCompanyInfo(e, t, n) {
         } 
         
     }
+    r.find(".channel").html("");
+    if(e["Channel"].length >= 1){
+        r.find(".channel").html("<strong>Distrobution Channel :</strong> "+e["Channel"].join(", ")); 
+    }
+    r.find(".orbit").html("");
+    if(e["Challenge Area"].length >= 1){
+        r.find(".orbit").html("<strong>Urban Impact :</strong> "+e["Challenge Area"].join(", ")); 
+    }    
 }
 
 function hideCompanyInfo() {
